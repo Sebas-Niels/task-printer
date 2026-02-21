d1a31477bde1be13ca32eca7fc191deca89eecf5
Sebastian Nielsen <sebas.nn@tuta.com>
2026-02-21 20:26:12 +0100

    Added flake for declaring the dev environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Simple Python dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # change if needed
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell {
+          packages = [
+            pkgs.python312
+            pkgs.python312Packages.pip
+            pkgs.python312Packages.virtualenv
+          ];
+        };
+    };
+}
